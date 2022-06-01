7faeffa7d4c3f80e7918fa66d07fe1963ff81394
Susu <xxx870421@gmail.com>
2022-06-01 14:09:29 +0800

    jQuery update

--- js/main.js
+++ js/main.js
@@ -2,33 +2,27 @@
 
 // 綁定 .ad-toggle-btn 的點擊事件
 $(".ad-toggle-btn").click(function () {
-  console.log("[.ad-toggle-btn被點擊了]");
+  $(".side-box").toggleClass("active");
 });
 
 // 綁定 #removeSideBoxBtn 的點擊事件
 $("#removeSideBoxBtn").click(function () {
-  console.log("[#removeSideBoxBtn被點擊了]");
+  $(".side-box").removeClass("active");
 });
 
 // 綁定 .navbar 裡面 .nav-link 的點擊事件
 $(".navbar .nav-link").click(function () {
-  console.log("[導覽列按鈕被點擊了]", this);
   // 取得滑動目標
-
   // 取得滑動目標的所在座標
-  if (this.hash !== "") {
-    let target = this.hash;
-    $("html, body").animate(
-      {
-        scrollTop: $(target).offset().top - 50,
-      },
-      800
-    );
-  }
 
-  // 設定動畫時間
+  const target = $(this).attr("href");
 
-  // 執行滑動動畫
+  $("html, body").animate(
+    {
+      scrollTop: $(target).stop().offset().top - 60,
+    },
+    800
+  );
 });
 
 $("#goBackBtn").click(function () {
