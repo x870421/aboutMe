b2e3bc8e1be736555a024a94186639614d3a8132
Susu <xxx870421@gmail.com>
2022-07-09 17:01:49 +0800

    update

--- js/main.js
+++ js/main.js
@@ -38,6 +38,15 @@ $(document).ready(function () {
   //navbar變色
   var navAnchor = document.getElementById("navtop");
   var img = document.getElementById("header");
+  if (window.pageYOffset != 0) {
+    navAnchor.style.backgroundColor = "black";
+    navAnchor.style.borderBottom = "none";
+    img.style.transition = "all 1s";
+    img.style.filter = " blur(5px)";
+  } else {
+    navAnchor.style = "";
+    img.style.filter = "";
+  }
 
   window.addEventListener("scroll", () => {
     if (window.pageYOffset != 0) {
@@ -58,7 +67,23 @@ $(document).ready(function () {
       $("#goBackBtn").fadeOut();
     }
   }
-  $(window).scroll(showBtnCondition);
+
+  const RWDupdate = document.querySelector(".RWDupdate");
+  const projectIntro = document.querySelectorAll(".project-intro");
+
+  if (window.innerWidth < 768) {
+    // 圖片動畫順序更正
+    console.log(RWDupdate);
+    RWDupdate.children[0].classList.add("bounceInLeft");
+    RWDupdate.children[0].classList.remove("bounceInRight");
+    RWDupdate.children[1].classList.add("bounceInRight");
+    RWDupdate.children[1].classList.remove("bounceInLeft");
+
+    //  圖片延遲修正
+    projectIntro.forEach((value) => {
+      value.setAttribute("data-wow-delay", "0s");
+    });
+  }
 
   new WOW().init();
 });
